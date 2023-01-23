b8bdb71d4f88456e6bb1b2ebd98a642ef975dbb0
Jan Vermeerbergen <janvermeerbergen@gmail.com>
2023-01-23 12:24:40 +0100

    Output changes, code cleanup

--- src/test/groovy/TestingProgram.groovy
+++ src/test/groovy/TestingProgram.groovy
@@ -3,12 +3,9 @@ import org.apache.camel.CamelContext
 import org.apache.camel.Exchange
 import org.apache.camel.impl.DefaultCamelContext
 import org.apache.camel.impl.DefaultExchange
+import org.apache.log4j.BasicConfigurator
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
-
-BasicConfigurator.configure();
-
+BasicConfigurator.configure()
 
 // Load Groovy Script
 GroovyShell shell = new GroovyShell()
@@ -33,20 +30,21 @@ script.processData(msg)
 exchange.getIn().setBody(msg.getBody())
 
 try {
-    FileWriter myWriter = new FileWriter('../../../data/out/output1.xml');
-    myWriter.write(msg.getBody(String));
-    myWriter.close();
-    //System.out.println("Successfully wrote to the file.");
+    FileWriter writer = new FileWriter('../../../data/out/output1.xml')
+    writer.write(msg.getBody(String))
+    writer.close()
 } catch (IOException e) {
-    System.out.println("An error occurred.");
-    e.printStackTrace();
+    System.out.println("An error occurred writing the output.")
+    e.printStackTrace()
 }
 
-
 // Display results of script in console
-println("Body:\r\n\r\n${msg.getBody(String)}")
+//println("Body:\r\n\r\n${msg.getBody(String)}")
 println()
+println("===============================================")
 println('Headers:')
 msg.getHeaders().each { k, v -> println("$k = $v") }
+println()
 println('Properties:')
 msg.getProperties().each { k, v -> println("$k = $v") }
+println("===============================================")
