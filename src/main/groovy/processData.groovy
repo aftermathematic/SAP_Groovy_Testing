7e58cc0d1905de856e3f56f1eebd20f5d9a4550e
aftermathematic <janvermeerbergen@gmail.com>
2023-02-02 13:48:57 +0100

    Update processData.groovy

--- src/main/groovy/processData.groovy
+++ src/main/groovy/processData.groovy
@@ -2,30 +2,40 @@ import com.sap.gateway.ip.core.customdev.util.Message
 import groovy.xml.MarkupBuilder
 
 Message processData(Message message) {
-    //Body
-    def body = message.getBody(String)as String
-    def parseXML = new XmlParser().parseText(body)
-    def varStringWriter = new StringWriter()
-    def varXMLBuilder   = new MarkupBuilder(varStringWriter)
 
-    String name
-    String email
-    int numberOfNodes = parseXML.Child.size()
+    /*
+    // Some much used code snippets
 
-    for(int i=0;i < numberOfNodes ;i++)
-    {
-        name = "${parseXML.Child[i].node1.text().toString()}"
-        email = "${parseXML.Child[i].node5.text().toString()}"
+    def body = message.getBody(String) as String
+    def root = new XmlParser().parseText(body)
 
-        varXMLBuilder.Employee{
-            Name(name)
-            Email(email)
+    //Iterate childnodes
+    root.Person.each { node ->
+        //Remove node
+        if (node.Name.text() == 'xxxxxxxxxxxxx') {
+            node.parent().remove(node)
         }
     }
 
-    def xml = varStringWriter.toString()
-    xml="<Employees>\n"+xml+"\n</Employees>"
-    message.setBody(xml)
+    //Remove node, lambda version
+    root.Person.findAll { it.Name.text() == 'John Doe' }.each { it.parent().remove(it) }
+
+    //Add new node to root
+    def nodeBuilder = new NodeBuilder()
+    def node = nodeBuilder.Person {
+        Name('Jan Vermeerbergen')
+        Age('42')
+        City('Wilrijk')
+    }
+    root.append(node)
+
+    //Sort the xml by attribute
+    root.Person.sort(true) { it.City.text() }
+
+    def outxml = XmlUtil.serialize(root)
+    message.setBody(outxml)
+*/
+
 
     return message
 }
